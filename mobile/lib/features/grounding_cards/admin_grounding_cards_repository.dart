eff557f2b9ac86bbde5b3f5ff5e289d608c8b1fa
ednezerrezende-svg <ednezer.rezende@creadev.com.br>
2026-08-06 19:48:29 -0300

    feat: add AdminGroundingCardsRepository

---
+++ mobile/lib/features/grounding_cards/admin_grounding_cards_repository.dart
@@ -0,0 +1,40 @@
+import 'package:dio/dio.dart';
+import 'grounding_card.dart';
+
+class AdminGroundingCardsRepository {
+  AdminGroundingCardsRepository(this._dio);
+
+  final Dio _dio;
+
+  Future<List<GroundingCard>> list() async {
+    final response = await _dio.get('/admin/grounding-cards');
+    return (response.data as List)
+        .map((json) => GroundingCard.fromJson(json as Map<String, dynamic>))
+        .toList();
+  }
+
+  Future<void> create({required String titulo, required String categoria, required String conteudo}) async {
+    await _dio.post('/admin/grounding-cards', data: {
+      'titulo': titulo,
+      'categoria': categoria,
+      'conteudo': conteudo,
+    });
+  }
+
+  Future<void> update(
+    String id, {
+    required String titulo,
+    required String categoria,
+    required String conteudo,
+  }) async {
+    await _dio.patch('/admin/grounding-cards/$id', data: {
+      'titulo': titulo,
+      'categoria': categoria,
+      'conteudo': conteudo,
+    });
+  }
+
+  Future<void> deactivate(String id) async {
+    await _dio.delete('/admin/grounding-cards/$id');
+  }
+}
--- mobile/lib/features/grounding_cards/grounding_cards_providers.dart
+++ mobile/lib/features/grounding_cards/grounding_cards_providers.dart
@@ -1,10 +1,15 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../core/api_providers.dart';
+import 'admin_grounding_cards_repository.dart';
 import 'grounding_card.dart';
 import 'grounding_cards_repository.dart';
 
 final groundingCardsRepositoryProvider = Provider<GroundingCardsRepository>((ref) {
   return GroundingCardsRepository(ref.watch(apiClientProvider).dio);
+});
+
+final adminGroundingCardsRepositoryProvider = Provider<AdminGroundingCardsRepository>((ref) {
+  return AdminGroundingCardsRepository(ref.watch(apiClientProvider).dio);
 });
 
 final groundingCardsProvider = FutureProvider.autoDispose.family<List<GroundingCard>, String?>((ref, categoria) {
